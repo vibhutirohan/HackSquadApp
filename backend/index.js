fd8d8757a2df8726adcfbf86c5b5e3d282c4a077
Mahendra-mohane <itsmahendramohane11@gmail.com>
2023-09-01 17:53:38 +0530

    backend structures

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,34 @@
-const express = require('express')
+const express = require("express");
+require("dotenv").config();
+const { Configuration, OpenAIApi } = require("openai");
+const path = require('path')
+const app = express();
+
+//Open AI configuration
+const openAi = new OpenAIApi(
+    new Configuration({
+      apiKey: process.env.OPEN_AI_API_KEY,
+    })
+  );
+const cors = require("cors");
+const { connection } = require("./config/db");
+const { InterviewModel } = require("./models/interviewModel");
+
+app.use(cors())
+app.use(express.json());
+let conversationHistory = [];
+
+
+
+
+//server part...//
+app.listen(4500,async()=>{
+    try {
+       await connection
+      console.log("Connected to DB")
+    } catch (error) {
+      console.log(error.message)
+    }
+    console.log("Server is running on port 4500")
+      
+  })
