6ebadb76ca32f2eb9a4a6246c4fc797fde20a59d
Mahendra-mohane <itsmahendramohane11@gmail.com>
2023-09-01 18:13:27 +0530

    some update

--- backend/index.js
+++ backend/index.js
@@ -17,6 +17,52 @@ const { InterviewModel } = require("./models/interviewModel");
 app.use(cors())
 app.use(express.json());
 let conversationHistory = [];
+const initialPrompts = [
+    {
+      role: "system",
+      content:"You are an interviewer. Ask 5 easy relevant questions about nodejs to the student, one at a time and keep the conversation going. If the student is unable to answer, move on to the next question without providing an explanation. After asking all the questions, provide feedback to the student regarding areas of improvement based on their answers."
+    },
+    {
+      role: "assistant",
+      content: "Great, let's start the interview."
+    }
+  ];
+
+  // Add initial prompts to the conversation history
+conversationHistory.push(...initialPrompts)
+
+
+app.post("/startprompt",async(req,res)=>{
+    const {prompt} = req.body;
+    conversationHistory.push({ role: "user", content: prompt })
+    const response = await openAi.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: conversationHistory,
+        max_tokens: 1000
+      });
+
+      const reply = response.data.choices[0].message.content.trim()
+
+      if (reply) {
+        if(conversationHistory.length===13){
+          conversationHistory.push({ role: "assistant", content: reply });
+          let feedback = conversationHistory[13].content;
+          feedback = feedback.split("\n")[1]
+          console.log(feedback)
+          let rating = response.data.choices[0].rating;
+          console.log(rating)
+           const newInterview = new InterviewModel({studentId:"1",role:"Node",conversation:conversationHistory,feedback:feedback,rating:rating})
+           await newInterview.save()
+           return res.status(200).send({status:200,res:reply,msg:"interview is done"})
+        }
+        else{
+          conversationHistory.push({ role: "assistant", content: reply });
+          return res.status(200).send({status:200,res:reply,msg:"ongoing"})
+        } 
+      }
+      return res.status(500).send({status:500,msg:"Try again after some time"})
+    
+})
 
 
 
